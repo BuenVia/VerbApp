935aea8775215289c88487553445c348782311d9
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-06-15 08:24:55 +0100

    updates to questions

--- js/questionSet.js
+++ js/questionSet.js
@@ -97,4 +97,42 @@ export const qs = {
             new quesTemp(w, tranSp, 'We had seen that film', { correct: 'Habíamos visto esa película', other: null}, 'Habíamos visto esa película')
         ]
     },
+    presCont: {
+        tense: 'Present Continuous',
+        questions : [
+            new quesTemp(mc, sel, 'correr', { correct: 'corriendo', other: ['corrido', 'corro']}, 'corriendo'),
+            new quesTemp(w, tranSp, 'We are ordering some drinks', { correct: 'Estamos pidiendo unas bebidas', other: null }, 'Estamos pidiendo unas bebidas'),
+            new quesTemp(mc, sel, 'hablar', { correct: 'hablando', other: ['hablaba', 'hablado']}, 'hablando'),
+            new quesTemp(w, tranSp, 'I am chatting with some friends', { correct: 'Estoy charlando con algunos amigos', other: null }, 'Estoy charlando con algunos amigos'),
+            new quesTemp(mc, sel, 'seguir', { correct: 'siguiendo', other: ['seguía', 'seguido']}, 'siguiendo')
+        ]
+    },
+    imFur: {
+        tense: 'Immediate Future',
+        questions : [
+            new quesTemp(w, tranEn, 'Van a estudiar para su examen', { correct: 'They are going to study for their exam', other: null}, 'Van a estudiar para su examen'),
+            new quesTemp(mc, sel, 'I am going to watch the football match', { correct: 'Voy a ver el partido del fútbol', other: ['Veré el partido del fútbol', 'Estoy viendo el partido del fútbol']}, 'Voy a ver el partido del fútbol'),
+            new quesTemp(w, tranEn, 'Voy a pedir dos botellas de agua', { correct: 'I am going to order two bottles of water', other: null}, 'Voy a pedir dos botellas de agua'),
+            new quesTemp(w, tranSp, 'You (inf., pl.) are going to close the window', { correct: 'Vais a cerrar la ventana', other: null}, 'Vais a cerrar la ventana'),
+            new quesTemp(mc, sel, 'Nosotros ________ a hacer la compra', { correct: 'vamos', other: ['van', 'vais']}, 'Nosotros vamos a hacer la compra')
+        ]
+    },
+    propFut: {
+        tense: 'Proper Future',
+        questions : [
+            new quesTemp(mc, sel, 'I will', { correct: 'veré', other: ['voy', 'haré']}, 'veré'),
+            new quesTemp(w, tranSp, 'I will go to the cinema', { correct: 'Iré al cine', other: null}, 'Iré al cine'),
+            new quesTemp(mc, sel, 'You (inf., sing.) will', { correct: 'verás', other: ['vas', 'van']}, 'verás'),
+            new quesTemp(w, tranEn, 'Mañana aprenderemos sobre las ventajas y las desventajas', { correct: 'Tomorow we will learn about the advantages and disadvantages', other: null}, 'Mañana aprenderemos sobre las ventajas y las desventajas'),
+            new quesTemp(w, tranSp, 'On Saturday you will sing the song you have written', { correct: 'El sábado tú cantarás la canción que has escrito', other: null}, 'El sábado tú cantarás la canción que has escrito')
+        ]
+    },
+    conditional: {
+        tense: 'Conditional',
+        questions : [
+            new quesTemp(mc, sel, 'I would', { correct: 'sería', other: ['haría', 'sea']}, 'sería'),
+            new quesTemp(mc, sel, 'They would', { correct: 'serían', other: ['harián', 'sean']}, 'serían'),
+            new quesTemp(w, tranSp, 'He would eat, but he isn\'t hungry', { correct: 'Comería, pero no tiene hambre', other: null}, 'Comería, pero no tiene hambre'),
+        ]
+    },
 }
